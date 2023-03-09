0951296a6e9461d8854ab5985e8c8be7363bf5e6
Pablo Banzo <pridapablo@gmail.com>
2023-03-09 11:53:55 -0600

    initalize javascript excercices

---
+++ js/A01782031&A01027384_javascript_exercises.js
@@ -0,0 +1,89 @@
+// 1. Escribe una función que encuentre el primer carácter de un cadena de texto que no se repite. Prueba tu función con: 'abacddbec'
+function findFirstNonRepeatedChar(str) {
+  console.log("slay");
+}
+console.log(findFirstNonRepeatedChar("abacddbec"));
+
+// 2. Escribe una función que implemente el algoritmo 'bubble-sort' para ordenar una lista de números.
+function bubbleSort(params) {
+  console.log("slay");
+}
+console.log(bubbleSort("abacddbec"));
+
+// 3. Escribe dos funciones: la primera que invierta un arreglo de números y regrese un nuevo arreglo con el resultado; la segunda que modifique el mismo arreglo que se pasa como argumento. No se permite usar la función integrada 'reverse'.
+function reverseArray(arr) {
+  console.log("slay");
+}
+console.log(reverseArray("abacddbec"));
+
+// 4. Escribe una función que reciba una cadena de texto y regrese una nueva con la primer letra de cada palabra en mayúscula.
+function capitalize(str) {
+  console.log("slay");
+}
+console.log(capitalize("abacddbec"));
+
+// 5. Escribe una función que calcule el máximo común divisor de dos números.
+function gcd(a, b) {
+  console.log("slay");
+}
+console.log(gcd(12, 18));
+
+// 6. Crea una función que cambie una cadena de texto a 'Hacker Speak'. Por ejemplo, para la cadena 'Javascript es divertido', su hacker speak es: 'J4v45c1pt 35 d1v3rt1d0'.
+function hackerSpeak(str) {
+  console.log("slay");
+}
+console.log(hackerSpeak("Hola, soy Pablo"));
+
+// 7. Escribe una función que reciba un número, y regrese una lista con todos sus factores. Por ejemplo: factoriza(12) -> [1, 2, 3, 4, 6, 12].
+function factorize(num) {
+  console.log("slay");
+}
+console.log(factorize(12));
+
+// 8. Escribe una función que quite los elementos duplicados de un arreglo y regrese una lista con los elementos que quedan. Por ejemplo: quitaDuplicados([1, 0, 1, 1, 0, 0]) -> [1, 0]
+function removeDuplicates(arr) {
+  console.log("slay");
+}
+console.log(removeDuplicates([1, 0, 1, 1, 0, 0]));
+
+// 9. Escribe una función que reciba como parámetro una lista de cadenas de texto, y regrese la longitud de la cadena más corta.
+function shortestString(arr) {
+  console.log("slay");
+}
+console.log(shortestString(["Hola", "soy", "Pablo"]));
+
+// 10. Escribe una función que revise si una cadena de texto es un palíndromo o no.
+function isPalindrome(str) {
+  console.log("slay");
+}
+console.log(isPalindrome("Hola, soy Pablo"));
+
+// 11. Escribe una función que tome una lista de cadena de textos y devuelva una nueva lista con todas las cadenas en orden alfabético.
+function alphabetize(params) {
+  console.log("slay");
+}
+console.log(alphabetize(["Hola", "soy", "Pablo"]));
+
+// 12. Escribe una función que tome una lista de números y devuelva la mediana y la moda.
+function centralTendencies(params) {
+  console.log("slay");
+}
+console.log(centralTendencies([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+// 13. Escribe una función que tome una lista de cadenas de texto y devuelva la cadena más frecuente.
+function mostFrequent(params) {
+  console.log("slay");
+}
+console.log(mostFrequent(["Hola", "soy", "Pablo"]));
+
+// 14. Escribe una función que tome un número y devuelva verdadero si es una potencia de dos, falso de lo contrario.
+function isPowerOfTwo(params) {
+  console.log("slay");
+}
+console.log(isPowerOfTwo(8));
+
+// 15. Escribe una función que tome una lista de números y devuelva una nueva lista con todos los números en orden descendente.
+function sortDescending(params) {
+  console.log("slay");
+}
+console.log(sortDescending([1, 2, 3, 4, 5, 6, 7, 8, 9]));
